c77ccec4d0f2bde832833b8bfd2dba70b2f06b02
Johanna Mergler <jmergler@stud.hs-heilbronn.de>
2018-03-29 08:52:26 +0200

    changed time scale to milliseconds

--- Aufgabe2/Aufgabe2.m
+++ Aufgabe2/Aufgabe2.m
@@ -5,10 +5,10 @@ ZeroDegreePosition = (61*2)-1;                      %because always left and rig
 ch1 = HRIR(1:end, ZeroDegreePosition:ZeroDegreePosition+1);
 %Plot HRIR
 figure
-t= (1: size(ch1)) * (1/Fs);
+t= (1: size(ch1)) * (1/Fs) * 10^3;
 plot(t, ch1);
 ylabel('Amplitude');
-xlabel('Time(s)');
+xlabel('Time (ms)');
 title('Ch 1 - 0° head rotation');
 
 
@@ -17,9 +17,9 @@ load('1deg_LSR4328P_ch02_48NLRNA_2400.mat');
 ch2 = HRIR(1:end, ZeroDegreePosition:ZeroDegreePosition+1);
 %Plot HRIR
 figure
-t= (1: size(ch2)) * (1/Fs);
+t= (1: size(ch2)) * (1/Fs) * 10^3;
 plot(t, ch2);
 ylabel('Amplitude');
-xlabel('Time(s)');
+xlabel('Time (ms)');
 title('Ch 2 - 0° head rotation');
 
